48a9abf46be50e71088fffb13f15982cc39a1aad
German Renato Cobian <germancobian@hotmail.com>
2022-07-26 09:55:38 -0700

    Define associations and validations for the different models

--- app/models/journeyman.rb
+++ app/models/journeyman.rb
@@ -1,2 +1,15 @@
 class Journeyman < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :skill, presence: true
+  validates :country, presence: true
+  validates :city, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+
+  validates :username, uniqueness: { case_sensitive: false }
 end

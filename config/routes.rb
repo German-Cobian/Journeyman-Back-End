2c66c53421f11a0c74cb1400498ea2bb0e81cfb7
German Renato Cobian <germancobian@hotmail.com>
2022-07-26 13:58:19 -0700

    Create User model, devise controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   namespace :v1, defaults: { format: 'json' } do
     resources :journeymen, only: [:index, :show, :create, :destroy]
     resources :reservations, only: [:index, :show, :create, :destroy]

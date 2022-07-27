e512b547052dc2275ff9cb852ab0efa9d4ec1ff8
German Renato Cobian <germancobian@hotmail.com>
2022-07-27 15:48:02 -0700

    Add configure_permitted_parameters to application-controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:username, :password) }
+  end
 end

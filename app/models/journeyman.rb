13725ae107b870a23b188cf14475a638d6e75683
German Renato Cobian <germancobian@hotmail.com>
2022-07-27 12:48:51 -0700

    Add has_one_attached and image_url to Journeyman model for journeyman_pic

--- app/models/journeyman.rb
+++ app/models/journeyman.rb
@@ -1,10 +1,15 @@
 class Journeyman < ApplicationRecord
   has_many :reservations, dependent: :destroy
+  has_one_attached :journeyman_pic
 
   validates :name, presence: true
   validates :skill, presence: true
   validates :country, presence: true
   validates :city, presence: true
   validates :price, presence: true
+
+  def image_url
+    Rails.application.routes.url_helpers.url_for(journeyman_pic) if journeyman_pic.attached?
+  end
 
 end
